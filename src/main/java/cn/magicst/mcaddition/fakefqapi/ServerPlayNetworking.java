8021648206380514fdb9c690f0da053130fca35c
麻瓜瓜 <master@magicst.cn>
2022-10-02 23:54:27 +0800

    [skip ci] Change : Add playnetworking

---
+++ src/main/java/cn/magicst/mcaddition/fakefqapi/ServerPlayNetworking.java
@@ -0,0 +1,12 @@
+package cn.magicst.mcaddition.fakefqapi;
+
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+
+public class ServerPlayNetworking {
+	public static void send(ServerPlayerEntity player, Identifier identifier, PacketByteBuf buf) {
+		player.networkHandler.sendPacket(new CustomPayloadS2CPacket(identifier, buf));
+	}
+}
