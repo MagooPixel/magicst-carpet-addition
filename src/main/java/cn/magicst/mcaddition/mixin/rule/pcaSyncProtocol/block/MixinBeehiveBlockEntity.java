744b0b122bfb7c28b8727f1b4fb6bcbdb852a8c1
麻瓜瓜 <master@magicst.cn>
2022-10-03 11:54:07 +0800

    Fix : List<BeehiveBlockEntity.BeeState> on Quiltimpl

--- src/main/java/cn/magicst/mcaddition/mixin/rule/pcaSyncProtocol/block/MixinBeehiveBlockEntity.java
+++ src/main/java/cn/magicst/mcaddition/mixin/rule/pcaSyncProtocol/block/MixinBeehiveBlockEntity.java
@@ -28,7 +28,7 @@ public abstract class MixinBeehiveBlockEntity extends BlockEntity {
     }
 
     @Inject(method = "tickBees", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;remove()V", shift = At.Shift.AFTER))
-    private static void postTickBees(World world, BlockPos pos, BlockState state, List<BeehiveBlockEntity.Bee> bees, BlockPos flowerPos, CallbackInfo ci) {
+    private static void postTickBees(World world, BlockPos pos, BlockState state, List<BeehiveBlockEntity.BeeState> bees, BlockPos flowerPos, CallbackInfo ci) {
         if (PcaSettings.pcaSyncProtocol && PcaSyncProtocol.syncBlockEntityToClient(Objects.requireNonNull(world.getBlockEntity(pos)))) {
             Main.LOGGER.debug("update BeehiveBlockEntity: {}", pos);
         }
